86f181bc8a70648781c8f5b0f534ae974894e056
Kelvin Luck <git@kelvinluck.com>
2009-10-13 17:42:46 +0100

    Using selects to allow selection of legal values for input of hasone fields...

--- classes/bamboo/field/hasone.php
+++ classes/bamboo/field/hasone.php
@@ -26,6 +26,17 @@ class Bamboo_Field_HasOne extends Bamboo_Field_Integer
 		}
 	}
 	
+	public function input($name, array $attr = NULL)
+	{
+		// TODO: cache?
+		$relatedItems = Bamboo::get_list($this->model);
+		$options = array();
+		foreach($relatedItems as $relatedItem) {
+			$options[$relatedItem->{$relatedItem->__id_field}->raw()] = $relatedItem->{$relatedItem->__name_field}->raw();
+		}
+		return Form::select($name, $options, $this->value, $attr);
+	}
+	
 	public function __toString()
 	{
 		return $this->get()->{$this->relatedModel->__name_field}->raw();
