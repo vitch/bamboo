436d6adc21364c3294150fdd7457ec28aa502bcc
unknown <Kelvin Luck@.(none)>
2009-10-15 19:10:44 +0100

    Added a class to date inputs to enable hook up of JS date picker

--- classes/bamboo/field/timestamp.php
+++ classes/bamboo/field/timestamp.php
@@ -4,6 +4,12 @@ defined('SYSPATH') or die('No direct script access.');
 class Bamboo_Field_Timestamp extends Bamboo_Field_Integer
 {
 	public $format = 'Y-m-d';
+    
+    public function input($name, array $attr = NULL)
+    {
+    	$attr['class'] = isset($attr['class']) ? $attr['class'] . ' date-picker' : 'date-picker';
+        return parent::input($name, $attr);
+    }
 	
 	public function __toString()
 	{
