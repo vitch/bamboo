3b3b26f9371333eaa84c0b8d3f5c86d848465693
unknown <Kelvin Luck@.(none)>
2009-10-15 19:32:55 +0100

    Making it so set timestamps actually works

--- classes/bamboo/field/timestamp.php
+++ classes/bamboo/field/timestamp.php
@@ -4,7 +4,17 @@ defined('SYSPATH') or die('No direct script access.');
 class Bamboo_Field_Timestamp extends Bamboo_Field_Integer
 {
 	public $format = 'Y-m-d';
-    
+	
+    public function set($value)
+    {
+    	// TODO: There must be a nicer way??
+    	if (((int)$value) . '' != $value) {
+    		return parent::set(strtotime($value));
+		} else {
+			return parent::set($value);
+		}
+    }
+	
     public function input($name, array $attr = NULL)
     {
     	$attr['class'] = isset($attr['class']) ? $attr['class'] . ' date-picker' : 'date-picker';
